8ad8912322387a47f7d276235583bc47acddda1a
emieli <emil.eliason@gmail.com>
2023-07-23 17:15:33 +0200

    Add generic new_message() to generate SSE messages
    Also added a !whoami command.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from queue import Queue, Full
 from flask import Flask, render_template, Response, request
+from datetime import datetime
+import json
 
 """
 https://maxhalford.github.io/blog/flask-sse-no-deps/
@@ -20,71 +22,50 @@ class MessageAnnouncer:
     def users(self) -> list[str]:
         """Return list of active users"""
         users = [listener.username for listener in listeners.values()]
-        return users
+        return sorted(users)
 
-    def build_sse_message(self, data: str | list, event: str = "") -> None | ValueError:
-        """Format data into SSE message."""
-        message = ""
-        if event:
-            message += f"event: {event}\n"
-
-        if isinstance(data, list):
-            """Multiple lines of data"""
-            message += "data: " + "\ndata: ".join(data)
-        elif isinstance(data, str):
-            """A single line of data"""
-            if "\n" in data:
-                raise ValueError("The data string must be a single line")
-            message += f"data: {data}"
-
-        print(message + "\n")
-        message += "\n\n"  # To signal that we have reached the end of the message
-        return message
-
-    def broadcast(self, data: str | list, event: str = "") -> None | ValueError:
+    def broadcast(self, message) -> None | ValueError:
         """Send message to all listeners"""
 
-        """Input validation"""
-        if not data:
+        if not message:
             raise ValueError("No data to send")
 
-        if not event:
-            chat_history.append(data)
-
-        """Build the SSE message"""
-        message = self.build_sse_message(data, event)
-
-        for i in reversed(list(listeners)):
+        for uuid in reversed(list(listeners)):
             """We loop in reverse as disconnected listeners are removed"""
             try:
-                listeners[i].queue.put_nowait(message)
+                listeners[uuid].queue.put_nowait(message)
             except Full:
-                del listeners[i]
+                username = listeners[uuid].username
+                del listeners[uuid]
+                message = new_message(f"{username} connection timed out.")
+                announcer.broadcast(message)
+                chat_history.append(message)
                 self.update_userlist()
 
-    def unicast(self, data: str | list, uuid: str, event: str = "") -> None | ValueError:
+    def unicast(self, uuid, message) -> None | ValueError:
         """Send message to a specific listener
         Multiple lines of data must be a list of strings
         A single line of data can be a string."""
 
         """Input validation"""
-        if not uuid:
-            raise ValueError("UUID must be given")
-        if not data:
-            raise ValueError("No data to send")
-
-        """Build the SSE message"""
-        message = self.build_sse_message(data, event)
+        if not message:
+            raise ValueError("No message to send")
 
         """Send SSE message"""
         try:
             listeners[uuid].queue.put_nowait(message)
         except Full:
+            username = listeners[uuid].username
             del listeners[uuid]
-            self.update_userlist()
+            message = new_message(f"{username} connection timed out.")
+            announcer.broadcast(message)
+            chat_history.append(message)
 
     def update_userlist(self):
-        self.broadcast(self.users, event="userlist")
+        users = " ".join(self.users)
+        sse_message = "event: userlist\n"
+        sse_message += f"data: {users}\n\n"
+        self.broadcast(sse_message)
 
 
 class CommandHandler:
@@ -95,40 +76,95 @@ class CommandHandler:
             case "!username":
                 self.username(args)
             case "!help":
-                message = [
-                    "Welcome to Chat, created by Golle.",
-                    "Available commands:",
-                    "  !username - Change your username",
-                ]
-                announcer.unicast(message, uuid)
+                self.help()
+            case "!whoami":
+                self.whoami()
             case _:
-                message = [
-                    f"{message} is an unknown command.",
-                    "Enter !help to see available commands.",
-                ]
-                announcer.unicast(message, uuid)
+                self.default()
+
+    def default(self):
+        message = "Unknown command. Enter !help to see available commands."
+        sse_message = new_message(message, send_timestamp=False)
+        announcer.unicast(self.uuid, sse_message)
+
+    def help(self):
+        message = [
+            "Welcome to Chat, created by Golle.",
+            "Available commands:",
+            "  !username - Change your username",
+            "  !whoami   - View your current username",
+        ]
+        sse_message = new_message(message, send_timestamp=False)
+        announcer.unicast(self.uuid, sse_message)
 
     def username(self, args):
         """Process the !username commmand"""
         if len(args) != 1:
-            help_message = [
-                f"!username {' '.join(args)}",
-                "This command allow you to change your username. It may not contain any spaces",
+            message = [
+                "This command allow you to change your username, with some restrictions:",
+                " - It may not contain any spaces",
+                " - It may not be longer than 12 characters",
                 "Example: !username Guest666",
             ]
-            announcer.unicast(help_message, self.uuid)
+            sse_message = new_message(message, send_timestamp=False)
+            announcer.unicast(self.uuid, sse_message)
             return
 
+        """Update username on server"""
+        new_username = args[0][:12]
         username = listeners[self.uuid].username
-        new_username = args[0]
-
         listeners[self.uuid].username = new_username
-        announcer.broadcast(f"{username} changed name to {new_username}")
+
+        """Update username on client"""
         announcer.update_userlist()
-        announcer.unicast(new_username, self.uuid, event="newUsername")
+        sse_message = new_username_message(new_username)
+        announcer.unicast(self.uuid, sse_message)
+
+        """Share new username with chat"""
+        sse_message = new_message(f"{username} changed name to {new_username}")
+        announcer.broadcast(sse_message)
+        chat_history.append(sse_message)
+
+    def whoami(self):
+        message = f"Your username is {listeners[self.uuid].username}."
+        sse_message = new_message(message, send_timestamp=False)
+        announcer.unicast(self.uuid, sse_message)
 
 
-chat_history = ["line one", "line two"]
+def new_message(message: str | list, user: str = "", send_timestamp: bool = True) -> str:
+    """Generate new message in SSE format"""
+    EVENT = "new_message"
+    END_OF_MESSAGE = "\n\n"
+    sse_message = f"event: {EVENT}\n"
+
+    data = {}
+    if user:
+        data["user"] = user
+    if send_timestamp:
+        data["timestamp"] = datetime.now().timestamp()
+
+    if isinstance(message, str):
+        data["message"] = message
+    if isinstance(message, list):
+        data["message"] = "\n".join(message)
+
+    sse_message += f"data: {json.dumps(data)}"
+    sse_message += END_OF_MESSAGE
+
+    return sse_message
+
+
+def new_username_message(username: str) -> str:
+    """Create 'new username' message in SSE format."""
+    EVENT = "new_username"
+    END_OF_MESSAGE = "\n\n"
+    sse_message = f"event: {EVENT}\n"
+    sse_message += f"data: {username}"
+    sse_message += END_OF_MESSAGE
+    return sse_message
+
+
+chat_history = []
 listeners = dict()
 announcer = MessageAnnouncer()
 
@@ -139,7 +175,7 @@ def index():
 
 
 @app.post("/")
-def post_message():
+def message_from_user():
     """Takes message from a user and sends it to all listeners using SSE.
     The SSE message must use a specific format, as shown below.
     The data keyword is mandatory.
@@ -154,17 +190,19 @@ def post_message():
 
     if "fuck" in message:
         """We send message as a "badword" event"""
-        message = "Server: Please don't swear."
-        announcer.unicast(message, uuid)
+        sse_message = new_message("Please don't swear.", send_timestamp=False)
+        announcer.unicast(uuid, sse_message)
         return {}, 200
 
     if message.startswith("!"):
+        sse_message = new_message(message)
+        announcer.unicast(uuid, sse_message)
         commandhandler = CommandHandler(message, uuid)
         return {}, 200
 
     """This is a normal SSE message"""
-    formatted_message = f"{user}: {message}"
-    announcer.broadcast(formatted_message)
+    sse_message = new_message(message, user)
+    announcer.broadcast(sse_message)
     return {}, 200
 
 
@@ -176,8 +214,7 @@ def listen(uuid_and_user=None):
     messages to a specific listener.
 
     When connecting, the listener also sends the username save in its browser
-    localStorage. If the value is null, the server generates a GuestNNN name.
-    """
+    localStorage. If the value is null, the server generates a GuestNNN name."""
 
     if not uuid_and_user:
         return "Error: uuid missing."
@@ -197,17 +234,32 @@ def listen(uuid_and_user=None):
                 username = guest_username
                 break
 
-    announcer.broadcast(data=f"{username} has joined the chat")
+    # This doesn't work because we don't log 'user joined' messages to chat_history.
+    sse_message = new_message(f"{username} has joined the chat")
+    announcer.broadcast(sse_message)
+    chat_history.append(sse_message)
 
     def stream():
         listener = Listener(uuid, username)
         listeners[uuid] = listener
         messages = listener.queue
 
-        last_hundred_messages = chat_history[-100:]
-        announcer.unicast(username, uuid, event="newUsername")
-        announcer.unicast(last_hundred_messages, uuid, event="connected")
-        announcer.unicast("Enter !help to see available commands.", uuid)
+        """Inform client about its new username"""
+        sse_message = new_username_message(username)
+        announcer.unicast(uuid, sse_message)
+
+        """Send last 100 user messages from chat history in a single SSE message"""
+        sse_message = ""
+        for message in chat_history[-100:]:
+            sse_message += message
+        announcer.unicast(uuid, sse_message)
+
+        """Send !help server message"""
+        sse_message = new_message(
+            f"Welcome {username}! Enter !help to see available commands.",
+            send_timestamp=False,
+        )
+        announcer.unicast(uuid, sse_message)
         announcer.update_userlist()
 
         while True:
